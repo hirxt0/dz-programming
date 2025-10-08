88a30afb372fb1cfad3eb495a90e03c0894672ba
hirxt0 <a88502336@gmail.com>
2025-10-08 22:00:14 +0500

    Update 08.10.py
    добавил 4 упражнение

--- 08.10.py
+++ 08.10.py
@@ -76,3 +76,54 @@ def game() -> str:
 print(game())
 
 # упражнение 4
+
+accounts = {}  
+
+def create_account(name: str):
+    if name in accounts:
+        print("такой счёт уже существует")
+    else:
+        accounts[name] = 0
+        print(f"счёт '{name}' создан")
+
+
+def deposit(name: str, amount: float):
+    if name not in accounts:
+        print("такого счёта нет")
+        return
+    
+    accounts[name] += amount
+    print(f"на счёт '{name}' внесено {amount}")
+
+
+def withdraw(name: str, amount: float):
+    if name not in accounts:
+        print("такого счёта нет")
+        return
+    
+    if accounts[name] < amount:
+        print("недостаточно средств")
+        return
+    
+    accounts[name] -= amount
+    print(f"со счёта '{name}' списано {amount}")
+
+
+def check_balance(name: str):
+    if name not in accounts:
+        print("Такого счёта не")
+        return
+    
+    print(f"баланс счёта '{name}': {accounts[name]}")
+
+
+def transfer(sender: str, receiver: str, amount: float):
+    if sender not in accounts or receiver not in accounts:
+        print("один из счетов не существует")
+        return
+    if accounts[sender] < amount:
+        print("недостаточно средств для перевода")
+        return
+    accounts[sender] -= amount
+    accounts[receiver] += amount
+    print(f"переведено {amount} со счёта '{sender}' на '{receiver}'")
